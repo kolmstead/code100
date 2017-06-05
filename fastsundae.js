5121871f90b4cec5c04c460fe3452bbf21a13d5f
kolmstead <olmstead.kent@gmail.com>
2017-06-05 03:29:31 +0000

    Yes I coded on Sunday but got button to work

--- fastsundae.js
+++ fastsundae.js
@@ -1,12 +1,11 @@
-const isaiah = "Inside<br>&nbsp starvation<br>&nbsp&nbsp  are<br>&nbsp&nbsp&nbsp  individual<br>&nbsp&nbsp&nbsp&nbsp &nbspattitudes.<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Hippocritical<br>&nbsp&nbsp&nbsp&nbsp attitudes.<br>&nbsp&nbsp&nbsp Individuals<br>&nbsp&nbsp are<br>&nbsp starving<br>inside.";
 
 function addChiasmus(){
-  document.getElementById('joe').innerHTML=`${isaiah2}`;
+  document.getElementById('joe').innerHTML=`${isaiah}`;
 }
 
-document.getElementById('joe').addEventListener('click',addChiasmus,false);
+document.getElementById('seeChiasmus').addEventListener('click',addChiasmus,false);
 
-const isaiah2 = `
+const isaiah = `
 <div>
 <p id='ch'>Inside</p>
 <p id='ch'>&nbsp starvation</p>
