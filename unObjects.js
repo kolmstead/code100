45a57b68ee590d527ae8bc2c5c5806ce0846c7a7
kolmstead <olmstead.kent@gmail.com>
2017-06-26 04:50:09 +0000

    show random UN pair on click

---
+++ unObjects.js
@@ -0,0 +1,120 @@
+//UN object data
+
+const un1 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN1poverty.png',
+  label: 'Poverty'
+};
+
+const un2 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN2hunger.png',
+  label: 'Hunger'
+};
+
+const un3 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN3health.png',
+  label: 'Health'
+};
+
+const un4 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN4education.png',
+  label: 'Education'
+};
+
+const un5 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN5gender.png',
+  label: 'Gender Equality'
+};
+
+const un6 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN6water.png',
+  label: 'Water'
+};
+
+const un7 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN7energy.png',
+  label: 'Energy'
+};
+
+const un8 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN8work.png',
+  label: 'Work'
+};
+
+const un9 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN9industry.png',
+  label: 'Industry'
+};
+
+const un10 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN10inequality.png',
+  label: 'Reduce Inequality'
+};
+
+const un11 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN11cities.png',
+  label: 'Cities & Towns'
+};
+
+const un12 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN12consumption.png',
+  label: 'Consumption'
+};
+
+const un13 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN13climate.png',
+  label: 'Climate'
+};
+
+const un14 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN14lifeBelowWater.png',
+  label: 'Life Below Water'
+};
+
+const un15 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN15lifeOnLand.png',
+  label: 'Life on Land'
+};
+
+const un16 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN16peace.png',
+  label: 'Peace'
+};
+
+const un17 = {
+  score: 0,
+  pairs: [],
+  image: 'UNimages/UN17partnerships.png',
+  label: 'Partnership'
+};
